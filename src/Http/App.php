75ac18af2d4336732892e8e405eeaf3818e1be8b
Marcos Paliari <marcos@paliari.com.br>
2019-04-28 00:44:22 -0300

    add Http

---
+++ src/Http/App.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Paliari\Utils\Http;
+
+use Paliari\Utils\A,
+    DomainException,
+    Exception;
+
+class App
+{
+
+    protected $request;
+    protected $response;
+
+    protected $routes = [];
+
+    public function __construct()
+    {
+        if (!isset($GLOBALS['X-Time'])) {
+            $GLOBALS['X-Time'] = microtime(true);
+        }
+        $this->response = new Response();
+        $this->request  = new Request();
+    }
+
+    /**
+     * @param string   $method HTTP method: GET, POST, PUT, PATCH, DELETE, OPTIONS, HEADER
+     * @param string   $route  eg: /users/{id}
+     * @param callable $callable
+     */
+    public function map($method, $route, $callable)
+    {
+        $this->routes[strtoupper($method)][$this->routeToPattern($route)] = $callable;
+    }
+
+    protected function routeToPattern($route)
+    {
+        $replace = '([\w\-_]+)';
+        preg_match_all('/{[\w]+}+/', $route, $a);
+        foreach ($a[0] as $search) {
+            $route = str_replace($search, $replace, $route);
+        }
+        $route = str_replace('/', '\/', $route);
+
+        return '!^' . $route . '$!';
+    }
+
+    public function run()
+    {
+        try {
+            $this->invoke();
+        } catch (Exception $e) {
+            if (200 == $this->response->code) {
+                $this->response->code = 409;
+            }
+            $this->response->output = $this->toJson(['error' => $e->getMessage()]);
+        }
+        echo $this->response;
+    }
+
+    protected function invoke()
+    {
+        $patterns = A::get($this->routes, $this->request->getMethod(), []);
+        foreach ($patterns as $pattern => $callable) {
+            if (preg_match($pattern, $this->request->getPath(), $p)) {
+                $params = array_slice($p, 1);
+                $args   = array_merge([$this->request, $this->response], $params);
+
+                return call_user_func_array($callable, $args);
+            }
+        }
+        $this->response->code = 404;
+        throw new DomainException('Page not found!');
+    }
+
+    protected function toJson($content)
+    {
+        return json_encode($content);
+    }
+
+}
---
+++ src/Http/Request.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Paliari\Utils\Http;
+
+use Paliari\Utils\A;
+
+class Request
+{
+
+    protected $_payload;
+
+    protected $_headers = [];
+
+    public function getPath()
+    {
+        return explode('?', $this->getRequestUri())[0];
+    }
+
+    public function getRequestUri()
+    {
+        return A::get($_SERVER, 'REQUEST_URI', '/');
+    }
+
+    public function getMethod()
+    {
+        return A::get($_SERVER, 'REQUEST_METHOD', 'GET');
+    }
+
+    public function getBody($key = null)
+    {
+        if (null === $this->_payload) {
+            $this->_payload = json_decode(file_get_contents('php://input'), true) ?: [];
+        }
+        if ($key) {
+            return A::deepKey($this->_payload, $key);
+        }
+
+        return $this->_payload;
+    }
+
+    public function post($key = null)
+    {
+        if ($key) {
+            return A::deepKey($_POST, $key);
+        }
+
+        return $_POST;
+    }
+
+    public function get($key = null)
+    {
+        if ($key) {
+            return A::deepKey($_GET, $key);
+        }
+
+        return $_GET;
+    }
+
+    public function userAgent()
+    {
+        return A::get($_SERVER, 'HTTP_USER_AGENT', '');
+    }
+
+    public function ip()
+    {
+        return A::get($_SERVER, 'REMOTE_ADDR', '');
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function getHeader($key)
+    {
+        return A::get($this->getHeaders(), $key);
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        if (!$this->_headers) {
+            if (function_exists('getallheaders')) {
+                $this->_headers = getallheaders();
+            } else {
+                foreach ($_SERVER as $k => $v) {
+                    if ('HTTP_' === substr($k, 0, 5)) {
+                        $this->_headers[$k] = $v;
+                    }
+                }
+            }
+        }
+
+        return $this->_headers;
+    }
+
+}
